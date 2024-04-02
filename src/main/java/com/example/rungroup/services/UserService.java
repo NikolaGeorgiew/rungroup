2971cd69c7d6ece0bfb8b51975bfe5f86a92b2a8
NikolaGeorgiew <nikolageorgiev2000n@gmail.com>
2024-04-02 18:54:31 +0300

    Implementing Register

--- src/main/java/com/example/rungroup/services/UserService.java
+++ src/main/java/com/example/rungroup/services/UserService.java
@@ -1,7 +1,12 @@
 package com.example.rungroup.services;
 
 import com.example.rungroup.dto.RegistrationDTO;
+import com.example.rungroup.models.UserEntity;
 
 public interface UserService {
     void saveUser(RegistrationDTO registrationDTO);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
